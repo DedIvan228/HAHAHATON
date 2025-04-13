5663e92c833b7d0864542d408ce3a205424241fe
Ivan <eeivan2208@gmail.com>
2025-04-13 14:01:50 +0300

    update settings.py

--- hackaton/settings.py
+++ hackaton/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import dj_database_url  # Импортируем библиотеку для работы с базой данных
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +53,10 @@ WSGI_APPLICATION = "hackaton.wsgi.application"
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))  # Используем переменную DATABASE_URL
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Путь к файлу базы данных
+    }
 }
 
 # Password validation
