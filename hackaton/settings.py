641277f69656810662568ee044af5c53321bc812
Ivan <eeivan2208@gmail.com>
2025-04-13 12:37:38 +0300

    update profile

--- hackaton/settings.py
+++ hackaton/settings.py
@@ -125,4 +125,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "app.User"
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/media')
