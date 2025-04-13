2ebd34c46d7313f0fe82bf3c5cc4d9b7a7b79023
Ivan <eeivan2208@gmail.com>
2025-04-13 13:25:36 +0300

    update settings

--- hackaton/settings.py
+++ hackaton/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/opt/render/project/src/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
